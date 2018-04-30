dca94de36c73d79dbe791c53267ff3fab41db16d
Daniel <dani.mualem@gmail.com>
2018-04-30 17:11:13 +0300

    server added

---
+++ DealsApp_server/models/deal.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Deal = new Schema({
+    storeOwnerID: {type: Schema.ObjectId, ref: 'StoreOwner', required: true},
+    storeID: {type: Schema.ObjectId, ref: 'Store', required:true},
+    details: {type: String, required: true},
+    time: String,
+    imgURL : String
+});
+
+module.exports = mongoose.model('Deal', Deal);
