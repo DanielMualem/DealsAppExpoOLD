73a2fbd59a458f110605ee46bc75a5ad46c654f5
Daniel <dani.mualem@gmail.com>
2018-05-07 00:53:28 +0300

    update 6.5

--- DealsApp_server/models/store.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Store = new Schema({
-    name: {type: String, required: true},
-    location: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Store', Store);
